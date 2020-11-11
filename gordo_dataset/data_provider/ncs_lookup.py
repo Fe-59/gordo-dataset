7ce2376bfb55d5158e642a2af714349e69add587
Serhii Koropets <skoropets@gmail.com>
2020-11-11 16:46:07 +0200

    Fixing KeyError in NcsLookup

--- gordo_dataset/data_provider/ncs_lookup.py
+++ gordo_dataset/data_provider/ncs_lookup.py
@@ -139,7 +139,6 @@ class NcsLookup:
         storage = self.storage
         ncs_file_types = self.ncs_file_types
         tag_name = self.quote_tag_name(tag.name)
-        not_existing_years = set(years)
         locations = {}
         for year in years:
             found = False
@@ -152,7 +151,6 @@ class NcsLookup:
                         found = True
                         break
                 if found:
-                    not_existing_years.remove(year)
                     break
         return TagLocations(tag, locations if locations else None)
 
