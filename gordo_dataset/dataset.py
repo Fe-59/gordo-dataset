0c3e516f1053318cb94bafa1020e989e7faed45e
Serhii Koropets <33310880+koropets@users.noreply.github.com>
2020-12-24 17:32:15 +0200

    Fixing issue with gordo AttributeError: module 'gordo_dataset.datasets' has no attribute (#27)
    * Fixing issue with gordo AttributeError: module 'gordo_dataset.datasets' has no attribute ''
    
    * Fixing UnboundLocalError: local variable 'module' referenced before assignment
    
    * test_from_dict_with_empty_type()
    
    * 2.3.6

--- gordo_dataset/dataset.py
+++ gordo_dataset/dataset.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import importlib
+from .exceptions import ConfigException
 
 
 def _get_dataset(config):
@@ -11,11 +12,18 @@ def _get_dataset(config):
     if '.' in kind:
         module_name, class_name = kind.rsplit(".", 1)
         # TODO validate module_name
-        Dataset = getattr(importlib.import_module(module_name), class_name)
+        module = importlib.import_module(module_name)
+        if not hasattr(module, class_name):
+            raise ConfigException("Unable to find class %s in module %s" % (module_name, class_name))
+        Dataset = getattr(module, class_name)
     else:
         import gordo_dataset.datasets as datasets
+        if not kind:
+            kind = "TimeSeriesDataset"
+        if not hasattr(datasets, kind):
+            raise ConfigException("Unable to find class %s in module gordo_dataset.datasets" % kind)
         Dataset = getattr(datasets, kind)
     if Dataset is None:
-        raise ValueError(f'Dataset type "{kind}" is not supported!')
+        raise ConfigException(f'Dataset type "{kind}" is not supported!')
 
     return Dataset(**dataset_config)
